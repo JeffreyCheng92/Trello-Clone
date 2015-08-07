dc2124c65833f039d1c53b6a752faa10f6709d9c
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-08-07 11:58:43 -0700

    implement user auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,24 @@
 class SessionsController < ApplicationController
-
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by_credentials(params[:users][:username],
+                                     params[:users][:password])
+    if @user
+      log_in(@user)
+      redirect_to root_url
+    else
+      @user = User.new
+      flash.now[:errors] = ["Invalid Credentials"]
+      render :new
+    end
   end
 
   def destroy
+    log_out
+    redirect_to new_session_url
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :password, :username, :session_token, presence: true
+  validates :password_digest, :username, :session_token, presence: true, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
   attr_reader :password
   after_initialize :ensure_session_token
